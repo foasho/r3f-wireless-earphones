c1b8896f38f4fd84fee1b19f8a498a0a2811238b
foasho <foa.sho0111@outlook.jp>
2023-10-29 14:38:23 +0900

    fix: mobile

--- src/dom/Hearder.tsx
+++ src/dom/Hearder.tsx
@@ -9,16 +9,18 @@ export const Header = () => {
           <div className="w-12 h-12 rounded-full flex items-center justify-center">
             <img src="/logo.jpg" className="w-8 h-8 rounded-full select-none" />
           </div>
-          <span className="pl-2 text-2xl font-bold md:text-4xl select-none">
+          <span className="pl-2 text-2xl font-bold md:text-4xl text-gray-800 select-none">
             Earphones
           </span>
         </div>
         <div className="flex items-center">
           <a 
-            className="pr-3 select-none text-gray-900" href="https://dova-s.jp/bgm/play7330.html" 
+            className="pr-3 select-none text-gray-800" href="https://dova-s.jp/bgm/play7330.html" 
             target="_blank" rel="noreferrer"
           >
-              カナリアスキップ / まんぼう二等兵
+              カナリアスキップ 
+              <span className="hidden md:inline">/ まんぼう二等兵</span>
+              <span className="inline md:hidden">♪</span>
           </a>
         </div>
       </div>
