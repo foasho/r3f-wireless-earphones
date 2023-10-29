57fc8ab52949e67c08463689799f133aff888994
foasho <foa.sho0111@outlook.jp>
2023-10-29 13:21:29 +0900

    change: opacity

--- src/canvas/Pointer.tsx
+++ src/canvas/Pointer.tsx
@@ -1,7 +1,16 @@
-import { Html, useCursor } from "@react-three/drei";
+import { useCursor } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { useEffect, useRef, useState } from "react";
-import { Mesh } from "three";
+import { useRef, useState } from "react";
+import { Group, Mesh } from "three";
+
+const fract = (x: number) => {
+  return x - Math.floor(x);
+};
+
+// 値と値の間を取得する
+const lerp = (a: number, b: number, t: number) => {
+  return (1 - t) * a + t * b;
+};
 
 type PointerProps = {
   position: [number, number, number];
@@ -13,33 +22,41 @@ export const Pointer = (
   { 
     position,
     color = "#f0f8ff",
-    hoverColor = "#00bfff",
+    hoverColor = "#FFA500",
     onClick = () => {}
   }: PointerProps
 ) => {
 
+  const grp = useRef<Group>(null);
   const ringRef = useRef<Mesh>(null);
   const [onHovered, setOnHovered] = useState(false);
 
   useCursor(onHovered);
 
-  const fract = (x: number) => {
-    return x - Math.floor(x);
-  };
-
   useFrame((state, delta) => {
+    const time = state.clock.getElapsedTime();
     if (ringRef.current){
-      const time = state.clock.getElapsedTime();
       ringRef.current.scale.set(
-        0.8 + fract(time/2) * 0.75,
-        0.8 + fract(time/2) * 0.75,
+        0.8 + fract(time/2) * 0.35,
+        0.8 + fract(time/2) * 0.35,
         0.8,
       );
+    }
+    if (grp.current){
+      // childrenのopacityを変更する
+      grp.current.children.forEach((child) => {
+        if (child instanceof Mesh) {
+          // 0~1の間でsin波を生成
+          const t = Math.sin(time * 2) * 0.5 + 0.5;
+          child.material.opacity = lerp(0.4, 0.8, t);
+        }
+      });
     }
   });
 
   return (
     <group
+      ref={grp}
       position={position}
       onClick={onClick}
     >
@@ -49,15 +66,22 @@ export const Pointer = (
         rotation={[-Math.PI / 2, 0, 0]}
       >
         <circleGeometry args={[0.3, 32, 32]} />
-        <meshStandardMaterial color={onHovered ? hoverColor : color} />
+        <meshStandardMaterial 
+          color={onHovered ? hoverColor : color} 
+          transparent 
+        />
       </mesh>
       <mesh
         ref={ringRef}
         rotation={[-Math.PI / 2, 0, 0]}
         scale={0.8}
       >
-        <ringGeometry args={[0.4, 0.45, 32]} />
-        <meshStandardMaterial color={onHovered ? hoverColor : color} roughness={0.75} transparent />
+        <ringGeometry args={[0.4, 0.425, 32]} />
+        <meshStandardMaterial 
+          color={onHovered ? hoverColor : color} 
+          roughness={0.75} 
+          transparent 
+        />
       </mesh>
     </group>
   )
